634e81bfc23203f29890529b61bd72edf6745e4a
troxtele <105340164+troxtele@users.noreply.github.com>
2023-05-14 07:10:40 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const option = document.querySelector(".label");
 const options = document.querySelector(".options ul");
 const lists = [...options.querySelectorAll("li")];
+const sections = [...document.querySelectorAll("section")]
 const dates = [...document.querySelectorAll(".date")];
-const forms = [...document.querySelectorAll(".form")];
 const strategy = [...document.querySelectorAll(".strategy .items .item")];
+
 
 option.addEventListener("click", () => {
   option.querySelector(".arrow svg").classList.toggle("rotate");
@@ -15,9 +16,9 @@ lists.map((list) =>
   list.addEventListener("click", () => {
     lists.forEach((list) => list.classList.remove("active"));
     list.classList.add("active");
+    
   })
 );
-
 
 // const date = new Date();
 
@@ -29,44 +30,65 @@ lists.map((list) =>
 // const todaysDate = date.toLocaleString("en-IN", dateObj);
 // dates.map((date) => );
 
-
 // Strategy
 
 strategy.map((item) => {
-
   // Star
+  const stars = [...item.querySelectorAll(".star svg")];
+  stars.map((star) => {
+    star.addEventListener("click", () => {
+      stars[0].classList.toggle("hidden");
+      stars[1].classList.toggle("hidden");
+    });
+  });
 
-  const arrow = item.querySelector(".arrow svg")
+  // plus
+  const plus = item.querySelector(".plus svg");
+  const form = item.querySelector(".form");
+  plus.addEventListener("click", () => {
+    plus.classList.toggle("rotate");
+    form.classList.toggle("height");
+  });
+  // Arrow
+  const arrow = item.querySelector(".arrow svg");
+  const bottom = item.querySelector(".bottom");
   arrow.addEventListener("click", () => {
-    arrow.classList.toggle("rotate")
-    const form = item.querySelector(".form")
-    form.classList.toggle("height")
-  })
-})
+    arrow.classList.toggle("rotate");
+    bottom.classList.toggle("height");
+  });
 
-// Forms
-forms.map((form) => {
+  // Forms
   const cross = form.querySelector(".cross");
   cross.addEventListener("click", () => {
     form.classList.toggle("height");
+    plus.classList.remove("rotate");
   });
 });
 
-//-----JS for Price Range slider-----
-$(function () {
-  $("#slider-range").slider({
-    range: true,
-    min: 130,
-    max: 500,
-    values: [130, 250],
-    slide: function (event, ui) {
-      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-    },
+const ranges = [...document.querySelectorAll(".ranges .item")]
+
+console.log(ranges)
+
+ranges.map((range) => {
+  const rangeInput = range.querySelectorAll(".range-input input");
+  const progress = range.querySelector(".slider .progress");
+
+  let priceGap = 1000;
+  rangeInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+
+      if (maxVal - minVal < priceGap) {
+        if (e.target.className === "range-min") {
+          rangeInput[0].value = maxVal - priceGap;
+        } else {
+          rangeInput[1].value = minVal + priceGap;
+        }
+      } else {
+        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+      }
+    });
   });
-  $("#amount").val(
-    "$" +
-      $("#slider-range").slider("values", 0) +
-      " - $" +
-      $("#slider-range").slider("values", 1)
-  );
 });
